df40242747e96da7e36c3beb2f2d5e76262f5221
epointal <elisabeth.pointal@free.fr>
2017-12-19 16:51:52 +0100

    tests

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
 var_dump($_GET);
+var_dump(__DIR__."/cds/".$_GET ["cds"].".php");
+if(file_exists(__DIR__."/cds/".$_GET ["cds"].".php")){
+    echo "exists";
+    include_once "cds/".$_GET ["cds"].".php";
+}
