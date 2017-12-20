4912492aeb047d475921820787a2fb86282c8e9a
epointal <elisabeth.pointal@free.fr>
2017-12-20 16:47:08 +0100

    ?

--- center/bcmt.php
+++ center/bcmt.php
@@ -13,5 +13,41 @@ $conn_id = ftp_connect($ftp_server) or die("Impossible de se connecter au serveu
 //ob_end_clean();
 //echo $data;
 
-$handle = file_get_contents("ftp://ftp.bcmt.fr/DEFINITIVE/ams/yea/ams.year");
+//$handle = file_get_contents("ftp://ftp.bcmt.fr/DEFINITIVE/ams/yea/ams.year");
+
+function ftp_get_contents($ftp_stream, $remote_file, $mode=FTP_ASCII, $resume_pos=null){
+    $pipes=stream_socket_pair(STREAM_PF_UNIX, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP);
+    if($pipes===false) return false;
+    if(!stream_set_blocking($pipes[1], 0)){
+        fclose($pipes[0]); fclose($pipes[1]);
+        return false;
+    }
+    $fail=false;
+    $data='';
+    if(is_null($resume_pos)){
+        $ret=ftp_nb_fget($ftp_stream, $pipes[0], $remote_file, $mode);
+    } else {
+        $ret=ftp_nb_fget($ftp_stream, $pipes[0], $remote_file, $mode, $resume_pos);
+    }
+    while($ret==FTP_MOREDATA){
+        while(!$fail && !feof($pipes[1])){
+            $r=fread($pipes[1], 8192);
+            if($r==='') break;
+            if($r===false){ $fail=true; break; }
+            $data.=$r;
+        }
+        $ret=ftp_nb_continue($ftp_stream);
+    }
+    while(!$fail && !feof($pipes[1])){
+        $r=fread($pipes[1], 8192);
+        if($r==='') break;
+        if($r===false){ $fail=true; break; }
+        $data.=$r;
+    }
+    fclose($pipes[0]); fclose($pipes[1]);
+    if($fail || $ret!=FTP_FINISHED) return false;
+    return $data;
+}
+$handle = ftp_get_contents("ftp://ftp.bcmt.fr/DEFINITIVE/ams/yea/ams.year", "php://output");
+
 echo $handle;
