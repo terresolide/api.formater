5b28b9398538cd539a855d7724cc3bdec7132169
epointal <elisabeth.pointal@free.fr>
2018-03-12 14:36:04 +0100

    page test/catalogue

---
+++ tests/catalog-component-vjs/index.php
@@ -0,0 +1,13 @@
+<?php
+include_once '../../config.php';
+$title = "Test Catalog Component";
+$content = APP_DIR."/contents/catalog-page.phtml";
+$class= "ft-catalogue";
+
+
+include TEMPLATE_DIR."/layout.phtml";
+
+
+
+
+
