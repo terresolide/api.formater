fc72950eeea1f1c584143934c112bc710f729923
epointal <elisabeth.pointal@free.fr>
2018-04-17 16:58:04 +0200

    dernière version catalogue

--- tests/geotiff-visualizer/page2.php
+++ tests/geotiff-visualizer/page2.php
@@ -1,6 +1,6 @@
 <?php
 include_once '../../config.php';
-$title = "Intégration des geotiffs (version 2.15.1)";
+$title = "Intégration des geotiffs (version 2.15.2)";
 $content = APP_DIR."/contents/geotiff-page2.phtml";
 $class= "ft-catalogue";
 
