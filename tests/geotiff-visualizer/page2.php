c13e1aed3fc0da1dc3786ec5b95307132b4d4ff1
epointal <elisabeth.pointal@free.fr>
2018-04-18 16:50:10 +0200

    mise à jour pages de tests

--- tests/geotiff-visualizer/page2.php
+++ tests/geotiff-visualizer/page2.php
@@ -1,6 +1,6 @@
 <?php
 include_once '../../config.php';
-$title = "Intégration des geotiffs (version 2.15.2)";
+$title = "Intégration des geotiffs (version 2.16.0)";
 $content = APP_DIR."/contents/geotiff-page2.phtml";
 $class= "ft-catalogue";
 
