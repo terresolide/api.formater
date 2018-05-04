df54226a17ce1d8716b47565f1474e619703674c
epointal <elisabeth.pointal@free.fr>
2018-05-04 10:49:08 +0200

    test webcomponent pour Grenoble

--- tests/geotiff-visualizer/page2.php
+++ tests/geotiff-visualizer/page2.php
@@ -1,6 +1,6 @@
 <?php
 include_once '../../config.php';
-$title = "Intégration des geotiffs (version 2.19.0)";
+$title = "Intégration des geotiffs (version 2.20.0)";
 $content = APP_DIR."/contents/geotiff-page2.phtml";
 $class= "ft-catalogue";
 
