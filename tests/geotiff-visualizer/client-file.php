58b154fa7854d7e9df1468b9384e645c050c9699
epointal <elisabeth.pointal@free.fr>
2018-10-26 14:10:32 +0200

    ajout page test de visualisation geotiff poste utilisateur

---
+++ tests/geotiff-visualizer/client-file.php
@@ -0,0 +1,11 @@
+<?php
+include_once '../../config.php';
+$title = "Geotiff visualizer pour les fichiers de l'utilisateur";
+$content = APP_DIR."/contents/geotiff-visualizer-client.phtml";
+$class= "ft-catalogue";
+
+include TEMPLATE_DIR."/layout.phtml";
+
+
+
+
