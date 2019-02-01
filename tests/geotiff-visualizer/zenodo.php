a9a52f0bfed78cbe61d28b2ff77d92dcb8cb5e15
epointal <elisabeth.pointal@free.fr>
2019-02-01 09:45:08 +0100

    add test page for geotiff visualizer from zenodo identifiant

---
+++ tests/geotiff-visualizer/zenodo.php
@@ -0,0 +1,11 @@
+<?php
+include_once '../../config.php';
+$title = "VISUALISEUR A PARTIR D'UN IDENTIFIANT ZENODO DANS L'URL";
+$content = APP_DIR."/contents/geotiff-visualizer-zenodo.phtml";
+$class= "ft-catalogue";
+
+include TEMPLATE_DIR."/layout.phtml";
+
+
+
+
