f393804739878533ee559263b9dddb83d99973f0
epointal <elisabeth.pointal@free.fr>
2019-02-07 16:28:28 +0100

    add test page gep metadata viewer

---
+++ tests/geotiff-visualizer/gep.php
@@ -0,0 +1,11 @@
+<?php
+include_once '../../config.php';
+$title = "Pour visualiser les produits à partir d'une fiche de métadata de gep";
+$content = APP_DIR."/contents/gep-metadata-viewer.phtml";
+$class= "ft-catalogue";
+
+include TEMPLATE_DIR."/layout.phtml";
+
+
+
+
